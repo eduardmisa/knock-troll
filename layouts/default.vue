e4e5541d19329129ce90de19acc1ecc9384daf65
eduardmisa <eduardmisa@gmail.com>
2020-08-16 21:28:40 +0800

    initial

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app dark>
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+html {
+  overflow-x: hidden;
+  overflow-y: hidden;
+}
+
+::-webkit-scrollbar {
+  height: 0.0rem;
+  width: 0.0rem;
+}
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-progress-circular
+    class="ma-auto"
+    :size="100"
+    color="white"
+    indeterminate
+  ></v-progress-circular>
+</template>
+
+<script>
+export default {
+  head: {
+    title: "Welcome ~ Click to continue"
+  },
+  data () {
+    return {
+      audio: new Audio()
+    }
+  },
+  methods: {
+    PlayAudio () {
+      const app = this
+      if (app.audio.paused) {
+        app.audio.play();
+      }
+    }
+  },
+  async mounted () {
+    const app = this
+    app.audio.src = "/knock.mp3";
+    app.audio.autoplay = true;
+    app.audio.loop  = true;
+    app.audio.volume = 1
+    document.addEventListener("click", () => {
+      app.PlayAudio()
+    })
+  }
+}
+</script>
+
+<style scoped>
+.pager {
+  height: 100vh!important;
+  width: 100vw!important;
+}
+.pager-content {
+  margin: auto!important;
+}
+</style>
