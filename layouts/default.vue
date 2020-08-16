f57768041a63b095c9c73b6655019eeab26ed0d8
eduardmisa <eduardmisa@gmail.com>
2020-08-16 23:20:24 +0800

    ctx

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,7 +6,29 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      audio: new Audio()
+    }
+  },
+  methods: {
+    PlayAudio () {
+      const app = this
+      if (app.audio.paused) {
+        app.audio.play();
+      }
+    }
+  },
+  async mounted () {
+    const app = this
+    app.audio.src = "knock.mp3";
+    app.audio.autoplay = true;
+    app.audio.loop  = true;
+    app.audio.volume = 1
+    document.addEventListener("click", () => {
+      app.PlayAudio()
+    })
+  }
 }
 </script>
 
@@ -20,5 +42,4 @@ html {
   height: 0.0rem;
   width: 0.0rem;
 }
-
 </style>
